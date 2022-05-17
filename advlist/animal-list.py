61f1a7a5d71a70fbbc80d85cfb3b3479e5151649
Mirk <mlmirk@users.noreply.github.com>
2022-05-17 21:09:55 +0000

    about complex lists

---
+++ advlist/animal-list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+def main():
+    list1 = ["fox", "hen", "cat" , "dog", "yak"]
+    print(f"{list1[0]} {list1[1]} {list1[2]} {list1[3]} {list1[4]}  ")
+
+
+
+main()    
+
+
+
