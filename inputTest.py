d22efa8eb420b2a2a1970e2195c716af2f189e73
Mirk <mlmirk@users.noreply.github.com>
2022-05-17 19:57:31 +0000

    join and split

---
+++ inputTest.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+def main():
+
+    userName = input("what is your name? \n")
+    inputDate = input("what is is the day of the week? \n" )
+
+    print("Hello",  userName ,"Happy", inputDate )
+
+
+main()
+
+
+
+
