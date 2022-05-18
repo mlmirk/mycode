0e6aa819aa5f2b04c3c04b88c87e2eb7fc7fc18c
Mirk <mlmirk@users.noreply.github.com>
2022-05-18 16:27:58 +0000

    day 3 morning backup

---
+++ day3warmup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+Gambit={'Real Name':'Remy LeBeau',
+        'First Appearance':'Uncanny X-Men Annual #14 (July, 1990), Uncanny X-Men #266 (August, 1990)',
+        'Creators':['Chris Claremont','Jim Lee'],
+        'Team Affliations':['X-Factor','X-Men','Marauders','Horsemen of Apocalypse,'],'Base of Operations':'Salem Centre, New York'}
+
+print(f"Gambit aka {Gambit['Real Name']} was created by two gents {Gambit['Creators'][0]} and {Gambit['Creators'][1]} he first appeared in {Gambit['First Appearance'][0]} on team {Gambit['Team Affliations'][0]}! ")
+
+
+
+
---
+++ dicts/dict.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+captainAmerica= {
+        "Skills and Abilities" :[ "Master Tactician and Strategist",
+            " Master Martial Artist"," Master Shield Fighter",
+            " Advanced Military Operator"," Master Acrobat",
+            " Indomitable Will"," Professional Sketch Artist",
+            " Expert Marksman", "Expert Swordsman", "Weapons Proficiency",
+            " Multilingual", "Expert Vehicular Driver"
+            ] , 
+        'Name':"steve Rogers" ,
+        "First Appearance" : "Captain America Comics #1 (March, 1941)",
+        "creators" :"Joe Simon, Jack Kirby",
+        "base of operations" : "NYC"
+        }
+
+
+
+
+
+
+
+
+
+
+
+
